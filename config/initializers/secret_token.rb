d87eb8fbde54c7ce75b8a2c48679a19e845149d0
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2014-02-15 16:17:28 +0530

    Initial commit with Rails platform structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsCricket::Application.config.secret_key_base = '09f6f453b2dc37590a27fe479255931621c2c93aa38d4baf5e51d99d2b43e9f9dc22e6e05355d20fcbdda2927083f81639f9de832eae04f751d85c3f2ef4b795'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsCricket::Application.config.session_store :cookie_store, key: '_RailsCricket_session'
